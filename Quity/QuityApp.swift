cd78221a7e98a069c217d3d0d4be0f5dca76976b
ZDahir <zaidmdahir@gmail.com>
2024-08-27 13:40:18 -0400

    Initial Commit

---
+++ Quity/QuityApp.swift
@@ -0,0 +1,32 @@
+//
+//  QuityApp.swift
+//  Quity
+//
+//  Created by Zaid Dahir on 2024-08-27.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct QuityApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
